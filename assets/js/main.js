465e611591b221598d9fa0c36d6fba5921f9959e
moufid-elaroui <moufid.elaroui@gmail.com>
2021-11-22 21:59:47 +0100

    section projet revu et corrigé

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,6 +110,21 @@ themeButton.addEventListener("click", () => {
 });
 
 // PROJET SWIPER
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // mousewheel: true,
+  // keyboard: true,
+});
 
 
 
